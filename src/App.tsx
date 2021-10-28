64673684b374b4e0d0565cc2927eea7c39ece08a
Ronald Tomaz <ronaldtmprofile@gmail.com>
2021-10-28 13:33:27 -0300

    involved the application with DndProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import { Home } from "./pages/Home";
 import { GlobalStyles } from "./styles/Global";
 
 export function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Home />
       <GlobalStyles />
-    </>
+    </DndProvider>
   );
 }
