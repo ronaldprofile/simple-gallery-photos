d963c37a4d5793ca731e3bbb9d387986463357ee
Ronald Tomaz <ronaldtmprofile@gmail.com>
2021-11-01 11:20:06 -0300

    in the component the metadata of the image was consumed

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,17 +1,31 @@
 import { IoCloseOutline } from "react-icons/io5";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { Container, Content, GallerySlider } from "./styles";
+import { IPhoto } from "../../types/Photo";
+import { IMetadata } from "../../types/MetadataPhoto";
 
 interface IModalProps {
+  photo: IPhoto;
+  metaDataPhoto: IMetadata | undefined;
   setIsModalActive: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export function Modal({ setIsModalActive }: IModalProps) {
+export function Modal({ photo, metaDataPhoto, setIsModalActive }: IModalProps) {
+  // const formatDate = Date.parse(metaDataPhoto?.timeCreated);
+  // const formattedDate = new Intl.DateTimeFormat("pt-br", {
+  //   year: "numeric",
+  //   month: "long",
+  //   day: "2-digit"
+  // }).format(formatDate);
+
+  const formatFileSize = Number(metaDataPhoto?.size) / 1024;
+  const formattedFileSize = formatFileSize.toFixed() + " MB";
+
   return (
     <Container>
       <Content>
         <header>
-          <strong className="fileName">screenshot.3829329.png</strong>
+          <strong className="fileName">{photo?.name}</strong>
           <IoCloseOutline
             size={32}
             title="fechar modal"
@@ -24,13 +38,47 @@ export function Modal({ setIsModalActive }: IModalProps) {
             <IoIosArrowBack size={24} />
           </div>
 
-          <img
-            src="https://images.unsplash.com/photo-1593642532454-e138e28a63f4?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2MHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-            alt=""
-          />
+          <div className="photo-modal">
+            <img src={photo?.url} alt={photo?.name} />
+
+            <div className="modal-content">
+              <h2>Detalhes</h2>
+
+              <div className="details">
+                <ul key={photo.name}>
+                  <li>
+                    Nome do arquivo
+                    <span>{metaDataPhoto?.nameFile}</span>
+                  </li>
+                  <li>
+                    Tipo do arquivo
+                    <span>{metaDataPhoto?.contentType}</span>
+                  </li>
+                  <li>
+                    Tamanho do arquivo
+                    <span>{formattedFileSize}</span>
+                  </li>
+
+                  {/* <li>
+                    Criado
+                    <span> {formattedDate}</span>
+                  </li> */}
+                </ul>
+              </div>
+            </div>
+          </div>
 
           <div className="button">
             <IoIosArrowForward size={24} />
+          </div>
+
+          <div className="buttons-mobile">
+            <div className="button">
+              <IoIosArrowBack size={24} />
+            </div>
+            <div className="button">
+              <IoIosArrowForward size={24} />
+            </div>
           </div>
         </GallerySlider>
       </Content>
--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -8,6 +8,7 @@ export const Container = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
+  z-index: 2;
 
   background: rgba(0, 0, 0, 0.7);
 `;
@@ -30,6 +31,13 @@ export const Content = styled.div`
       cursor: pointer;
     }
   }
+
+  @media (max-width: 425px) {
+    header .fileName {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
 `;
 
 export const GallerySlider = styled.div`
@@ -38,16 +46,80 @@ export const GallerySlider = styled.div`
   align-items: center;
   justify-content: space-between;
 
-  img {
-    max-width: 100%;
-    max-height: 100%;
-    object-fit: cover;
+  .photo-modal {
+    max-width: 57.5rem;
+    padding: 0 0.75rem;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.4s ease-in-out;
+
+    &:hover {
+      .modal-content {
+        opacity: 1;
+        visibility: visible;
+        transform: scaleY(1);
+        transform-origin: left;
+      }
+    }
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      cursor: pointer;
+    }
+
+    .modal-content {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      opacity: 0;
+      visibility: hidden;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+
+      background-color: rgba(0, 0, 0, 0.8);
+      color: #fff;
+
+      transform: scaleY(0);
+      transition: transform 0.3s ease-in-out;
+      transform-origin: right;
+
+      .details {
+        margin-top: 2rem;
+        ul {
+          list-style: none;
+
+          display: flex;
+          flex-direction: column;
+          gap: 0.75rem;
+
+          li {
+            span {
+              margin-left: 0.75rem;
+              padding: 0.75rem;
+              display: inline-block;
+              font-size: 0.75rem;
+              background-color: #388bfd;
+              border-radius: 1.5rem;
+              transition: background-color 0.2s;
+              &:hover {
+                background-color: #58a6ff;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   .button {
-    height: 3.125rem;
-    width: 3.75rem;
-
+    padding: 0.875rem;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -66,6 +138,31 @@ export const GallerySlider = styled.div`
       color: #ffffff;
       cursor: pointer;
       transition: background-color 0.2s;
+    }
+  }
+
+  .buttons-mobile {
+    display: none;
+  }
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+    justify-content: center;
+    gap: 2rem;
+
+    .photo-modal {
+      padding: 0;
+    }
+
+    > .button {
+      display: none;
+    }
+
+    .buttons-mobile {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
     }
   }
 `;
